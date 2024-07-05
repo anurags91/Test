088b6704e7bd28b2558867969d97a8f89099d112
Anurag Sharma <166206935+anurags91@users.noreply.github.com>
2024-07-05 23:32:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mouseOver() {
+    document.getElementById("heading").style.color = "#FF9999";
+  }
+  
+  function mouseOut() {
+    document.getElementById("heading").style.color = "white";
+  }
+  function myFunction(){
+    document.getElementById("quote").innerHTML="The future belongs to those who believe in the beauty of their dreams.";
+  }
